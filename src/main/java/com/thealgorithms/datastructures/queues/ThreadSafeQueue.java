b2199c94ee2ec8b04cc4215dfe22c73641be3ca9
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-04-19 22:33:12 +0530

    style: fix formatting issues (clang-format)

--- src/main/java/com/thealgorithms/datastructures/queues/ThreadSafeQueue.java
+++ src/main/java/com/thealgorithms/datastructures/queues/ThreadSafeQueue.java
@@ -2,46 +2,46 @@ package com.thealgorithms.datastructures.queues;
 
 public class ThreadSafeQueue<T> {
 
-    private static class Node<T> {
-        T data;
-        Node<T> next;
+	private static class Node<T> {
+		T data;
+		Node<T> next;
 
-        Node(T data) {
-            this.data = data;
-        }
-    }
+		Node(T data) {
+			this.data = data;
+		}
+	}
 
-    private Node<T> head;
-    private Node<T> tail;
+	private Node<T> head;
+	private Node<T> tail;
 
-    public synchronized void enqueue(T data) {
-        Node<T> newNode = new Node<>(data);
+	public synchronized void enqueue(T data) {
+		Node<T> newNode = new Node<>(data);
 
-        if (tail == null) {
-            head = tail = newNode;
-            return;
-        }
+		if (tail == null) {
+			head = tail = newNode;
+			return;
+		}
 
-        tail.next = newNode;
-        tail = newNode;
-    }
+		tail.next = newNode;
+		tail = newNode;
+	}
 
-    public synchronized T dequeue() {
-        if (head == null) {
-            return null;
-        }
+	public synchronized T dequeue() {
+		if (head == null) {
+			return null;
+		}
 
-        T data = head.data;
-        head = head.next;
+		T data = head.data;
+		head = head.next;
 
-        if (head == null) {
-            tail = null;
-        }
+		if (head == null) {
+			tail = null;
+		}
 
-        return data;
-    }
+		return data;
+	}
 
-    public synchronized boolean isEmpty() {
-        return head == null;
-    }
+	public synchronized boolean isEmpty() {
+		return head == null;
+	}
 }
--- src/test/java/com/thealgorithms/datastructures/queues/ThreadSafeQueueTest.java
+++ src/test/java/com/thealgorithms/datastructures/queues/ThreadSafeQueueTest.java
@@ -2,25 +2,25 @@ package com.thealgorithms.datastructures.queues;
 
 public class ThreadSafeQueueTest {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        ThreadSafeQueue<Integer> queue = new ThreadSafeQueue<>();
+		ThreadSafeQueue<Integer> queue = new ThreadSafeQueue<>();
 
-        Thread producer = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                queue.enqueue(i);
-                System.out.println("Enqueued: " + i);
-            }
-        });
+		Thread producer = new Thread(() -> {
+			for (int i = 1; i <= 5; i++) {
+				queue.enqueue(i);
+				System.out.println("Enqueued: " + i);
+			}
+		});
 
-        Thread consumer = new Thread(() -> {
-            for (int i = 1; i <= 5; i++) {
-                Integer val = queue.dequeue();
-                System.out.println("Dequeued: " + val);
-            }
-        });
+		Thread consumer = new Thread(() -> {
+			for (int i = 1; i <= 5; i++) {
+				Integer val = queue.dequeue();
+				System.out.println("Dequeued: " + val);
+			}
+		});
 
-        producer.start();
-        consumer.start();
-    }
+		producer.start();
+		consumer.start();
+	}
 }
